ef9bc52aa1466740668014ada21fc80b74efe626
aiswarya <aiswaryavalsan@gmail.com>
2025-02-21 16:28:50 +0530

    login page and validation

--- src/Components/Authentication/Signup.jsx
+++ src/Components/Authentication/Signup.jsx
@@ -2,42 +2,55 @@ import React, { useRef, useState } from 'react'
 
 const Signup = () => {
     const[isLogin,setIsLogin]=useState(false);
+    const[error,setError]=useState('');
     const emailRef=useRef();
     const passwordRef=useRef();
+    const confirmPasswordRef=useRef();
     const toggleLoginHandler=()=>{
+        setError('');
         setIsLogin(prev=>!prev);
     }
-    const onSubmitHandler=(event)=>{
+    const onSubmitHandler=async(event)=>{
         event.preventDefault();
-        const email=emailRef.current.value;
-        const password=passwordRef.current.value;
-        console.log('hi')
-        console.log(isLogin)
-        if(isLogin){
+        setError('');
+        const email=emailRef.current.value.trim();
+        const password=passwordRef.current.value.trim();
+        const confirmPassword=confirmPasswordRef.current.value.trim();
+        console.log('1')
+        if(!email||!password){
+           
+            setError('Email and Password is Required')
+            return
+        }
+       if(!isLogin){
+        if(password.length<6){
+            setError('Password must be at least 6 characters long.');
+            return;
 
         }
-        else{
-            console.log('ji')
-            fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC4OHncylrw5Aq4NuqLugUwTFgkydeOz7k',
-             {method:'POST',body:JSON.stringify({email,password,returnSecureToken:true}),headers:{'Content-Type': 'application/json'}})
-             .then((response)=>{
-                if(!response.ok){
-                    throw new Error(response)
+        if(password!==confirmPassword){
+            setError('Passwords Do Not Match');
+            return;
+        }
+       }
+        
 
-                }
-                else{
-                   return response.json();
-                }
+       
+        try{
+        const url=isLogin?'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC4OHncylrw5Aq4NuqLugUwTFgkydeOz7k':'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC4OHncylrw5Aq4NuqLugUwTFgkydeOz7k' ;
+        const response=await fetch(url,{method:'POST',body:JSON.stringify({email,password,returnSecureToken:true}),headers:{'Content-Type': 'application/json'}})
+          const data=await response.json(); 
+          if(!response.ok){
+            throw new Error(data.error.message);
+          }
+          alert(isLogin?'Login Successful':'Sign up successful')
                
-             }).then(data=>{
-                console.log(data)
-             }).catch(e=>{
-                console.log(e);
-             })
+        }catch(error){
             
-            }
-
-    }
+           setError(error.message);
+        } 
+             
+        }
   return (
     <>
       
@@ -51,15 +64,16 @@ const Signup = () => {
         <h1  className="text-3xl font-bold text-gray-800 mb-6"> {isLogin?'Login':'Sign Up'}</h1>
         
            {!isLogin&&<input type="text" placeholder='Full Name'className= ' w-60 border-b border-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500'/>}
-            <input type="email" ref={emailRef} placeholder='Email' className=' w-60 border-b border-gray-500focus:outline-none focus:ring-2 focus:ring-blue-500'/>
+            <input type="email" ref={emailRef} placeholder='Email' className=' w-60 border-b border-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500'/>
             <input type="password" ref={passwordRef} placeholder='Password'className=' w-60 border-b border-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500'/>
-           {!isLogin&& <input type="password" placeholder='Confirm Password' className=' w-60 border-b border-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500'/>}
+           {!isLogin&& <input type="password" ref={confirmPasswordRef} placeholder='Confirm Password' className=' w-60 border-b border-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500'/>}
             <button  className="bg-blue-500 text-white font-bold w-30 rounded-lg hover:bg-blue-600 transition">
               {isLogin?'LOGIN':'SIGNUP'}
             </button>
-           
+            {error&&<p className='text-red-500'>{error}</p>}
            <br/>
            <br/>
+          
         </form>
         <div>
         <button onClick={toggleLoginHandler} className='text-white px-1 py-2 mb-3'>
